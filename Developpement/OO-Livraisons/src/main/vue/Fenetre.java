1184d2e35d1e7a449e8329706ef8ba2e45ec9fe2
WinterNis <nicolas_nativel_974@hotmail.fr>
2015-11-04 11:40:11 +0100

    resolution de bugs graphiques

--- Developpement/OO-Livraisons/src/main/vue/Fenetre.java
+++ Developpement/OO-Livraisons/src/main/vue/Fenetre.java
@@ -72,6 +72,7 @@ public class Fenetre extends JFrame {
     protected JButton calculerTournee;
 
     private Controleur controleur;
+    private Plan plan;
 
     public Fenetre(Controleur c) {
         controleur = c;
@@ -312,10 +313,10 @@ public class Fenetre extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
-            Plan monPlan = controleur.chargerPlan();
+            plan = controleur.chargerPlan();
             vueGraphique.removeAll();
-            vueGraphique.drawPlan(monPlan);
+            vueTextuelle.removeAll();
+            vueGraphique.drawPlan(plan);
             revalidate();
             repaint();
         }
@@ -332,7 +333,7 @@ public class Fenetre extends JFrame {
             } catch (Exception ex) {
                 Logger.getLogger(Fenetre.class.getName()).log(Level.SEVERE, null, ex);
             }
-
+            vueTextuelle.removeAll();
             vueTextuelle.writeLivraisons(livraisons,null);
             revalidate();
             repaint();
