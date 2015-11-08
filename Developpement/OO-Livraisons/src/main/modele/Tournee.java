30e5dd793cfde610b144348cd013b3aff1e85dce
Guillaume <guillaume.kheng@insa-lyon.fr>
2015-11-08 16:59:38 +0100

    Fixed: Tournee.AjouterChemin(Chemin). Now the tournee is correctly adding the chemins in the right order

--- Developpement/OO-Livraisons/src/main/modele/Tournee.java
+++ Developpement/OO-Livraisons/src/main/modele/Tournee.java
@@ -12,9 +12,10 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * Une tournée est un ensemble ordonné de chemins. Elle détermine la totalité 
- * du trajet du livreur depuis l'entrepôt jusqu'à l'entrepôt, en passant par 
- * les points de livraison.
+ * Une tournée est un ensemble ordonné de chemins. Elle détermine la totalité du
+ * trajet du livreur depuis l'entrepôt jusqu'à l'entrepôt, en passant par les
+ * points de livraison.
+ *
  * @author tfavrot
  */
 public class Tournee {
@@ -50,9 +51,9 @@ public class Tournee {
 
             // on regarde si on se trouve dans la fenetre de la prochaine livraison.
             if (cheminCourant.getLivraisonArrivee().getFenetreLivraison().getHeureDebut().compareTo(instantCourant) < 0) { // si c'est le cas, on ivre.
-                instantCourant.setTime(instantCourant.getTime() + cheminCourant.getLivraisonArrivee().getTempsArret()*1000);
+                instantCourant.setTime(instantCourant.getTime() + cheminCourant.getLivraisonArrivee().getTempsArret() * 1000);
             } else { // sinon on attend le debut de fenetre puis on livre avant de continuer
-                instantCourant.setTime(cheminCourant.getLivraisonArrivee().getFenetreLivraison().getHeureDebut().getTime() + cheminCourant.getLivraisonArrivee().getTempsArret()*1000);
+                instantCourant.setTime(cheminCourant.getLivraisonArrivee().getFenetreLivraison().getHeureDebut().getTime() + cheminCourant.getLivraisonArrivee().getTempsArret() * 1000);
             }
             cheminCourant.getLivraisonArrivee().setHeureLivraison(instantCourant);
         }
@@ -63,38 +64,53 @@ public class Tournee {
 
     /**
      * Retourne la liste des chemins qui composent la tournée.
+     *
      * @return La liste des chemins qui composent la tournée.
      */
     public Iterator<Chemin> getChemins() {
         List<Chemin> constList = Collections.unmodifiableList(chemins);
         return constList.iterator();
     }
-    
+
     /**
      * Permet de supprimer chemin de la liste des chemins
-     * @param chemin 
+     *
+     * @param chemin
      */
-    public void supprimerChemin(Chemin chemin){
+    public void supprimerChemin(Chemin chemin) {
         this.chemins.remove(chemin);
-        
+
         // on met à jour les heures de livraison
         this.CalculerHeuresDemandesLivraisons();
     }
+
     /**
      * Ajoute un chemin à la collection de chemins. Cette fonction vérifie que
-     * le chemin passé en paramètre n'est pas "null".
+     * le chemin passé en paramètre n'est pas "null". Cette fonction ajoute le
+     * chemin "au bon endroit"
      *
      * @param chemin le chemin à ajouter à la collection
      * @return Le chemin si les conditions d'ajout sont respectées, null sinon.
      */
     public Chemin AjouterChemin(Chemin chemin) {
-        if (chemin != null) {
-            this.chemins.add(chemin);
+
+        if (chemin == null) {
+            return null;
         }
-        
+
+        Iterator<Chemin> it_chemin = this.chemins.iterator();
+        int i = 0;
+        while (it_chemin.hasNext()) {
+            if (it_chemin.next().getIntersectionDepart().getId() == chemin.getIntersectionArrivee().getId()) {
+                break;
+            }
+            i++;
+        }
+        this.chemins.add(i, chemin);
+
         // on met à jour les heures de livraison
         this.CalculerHeuresDemandesLivraisons();
-        
+
         return chemin;
     }
 
